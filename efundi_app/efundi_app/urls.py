9b39f42ae87ea9d22a9d20466917055dd5824933
stepholo <oloobrian89@gmail.com>
2026-06-08 14:23:32 +0300

    Adding django settings

---
+++ efundi_app/efundi_app/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for efundi_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.urls import include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import PasswordResetConfirmPageView
+
+urlpatterns = [
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('admin-auth/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('reset-password/', PasswordResetConfirmPageView.as_view(), name='password-reset-page'),
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/bookings/', include('bookings.urls')),
+    path('api/v1/clients/', include('customers.urls')),
+    path('api/v1/common/', include('common.urls')),
+    path('api/v1/notifications/', include('notifications.urls')),
+    path('api/v1/payments/', include('payments.urls')),
+    path('api/v1/reviews/', include('reviews.urls')),
+    path('api/v1/technicians/', include('technicians.urls')),
+    path('api/v1/commissions/', include('commissions.urls')),
+]
